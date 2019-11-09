4c463444fb81901f94899f20e5cab488931c6649
SeungHyun Lee <xion@kaist.ac.kr>
2019-11-10 02:03:02 +0900

    191109 AppContainer SEHOP issues :(
    chal binary running inside AppContainer seems to fail loading SEHOP-disabling registry keys, causing SEHOP to be in effect

--- TIL/191109/bartender/run_server.bat
+++ TIL/191109/bartender/run_server.bat
@@ -3,6 +3,7 @@
 set RUST_LOG=debug
 set PORT=12345
 
-..\..\..\Tools\appjaillauncher-rs\target\i686-pc-windows-msvc\debug\appjaillauncher-rs.exe run .\bartender.exe -k .\flag -p %PORT%
+echo Running on debug due to SEHOP registry issues, connection only succeeds once :(
+..\..\..\Tools\appjaillauncher-rs\target\i686-pc-windows-msvc\debug\appjaillauncher-rs.exe run .\bartender.exe -k .\flag -p %PORT% --debug
 
 pause
