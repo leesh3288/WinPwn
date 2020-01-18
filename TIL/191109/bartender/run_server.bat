ee5f5290ca7fe53ad63a255b7e32501c1cbe617d
SeungHyun Lee <xion@kaist.ac.kr>
2020-01-19 00:01:34 +0900

    minimalize ECV patch

--- TIL/191109/bartender/run_server.bat
+++ TIL/191109/bartender/run_server.bat
@@ -1,6 +1,5 @@
 @echo off
 
-REM set RUST_LOG=debug
 set PORT=12345
 
 ..\..\..\Tools\appjaillauncher-rs\target\i686-pc-windows-msvc\debug\appjaillauncher-rs.exe run .\bartender_patched.exe -k .\flag -p %PORT%
